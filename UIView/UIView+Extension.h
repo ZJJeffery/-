a84e8be00a6f8fe9b082351e13c8dd53fa487dc5
郑佳俊 <359191701@qq.com>
2015-05-06 19:16:25 +0800

    NSString NSArray UIView

---
+++ UIView/UIView+Extension.h
@@ -0,0 +1,22 @@
+
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Extension)
+/**
+ *  控件frame的x属性
+ */
+@property (nonatomic, assign) CGFloat x;
+/**
+ *  控件frame的y属性
+ */
+@property (nonatomic, assign) CGFloat y;
+/**
+ *  控件frame的width属性
+ */
+@property (nonatomic, assign) CGFloat w;
+/**
+ *  控件frame的height属性
+ */
+@property (nonatomic, assign) CGFloat h;
+@end
